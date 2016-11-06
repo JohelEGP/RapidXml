111a3565a607d9b258a3aa71d1397cbaf44b6691
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2016-11-06 17:44:47 -0400

    Add test for the ranges

---
+++ tests/test_ranges/main.cpp
@@ -0,0 +1,42 @@
+#include "../../rapidxml_iterators.hpp"
+#include "../test_utils.hpp"
+#include <vector>
+
+using namespace std;
+using namespace rapidxml;
+
+int main()
+{
+    vector<string_view> names {"elem1","elem2","attr1","attr2"};
+    vector<string_view> values {"elemval1","elemval2","attrval1","attrval2"};
+
+    auto name_it {names.begin()};
+    auto value_it {values.begin()};
+
+    xml_document<char> doc;
+    xml_node<char> *node1 = doc.allocate_node(node_element, *name_it++, *value_it++);
+    xml_node<char> *node2 = doc.allocate_node(node_element, *name_it++, *value_it++);
+    node1->append_attribute(doc.allocate_attribute(*name_it++, *value_it++));
+    node2->append_attribute(doc.allocate_attribute(*name_it++, *value_it++));
+    doc.append_node(node1);
+    doc.append_node(node2);
+
+    auto elem_name_it {names.begin()};
+    auto attr_name_it {names.begin()+2};
+    auto elem_value_it {values.begin()};
+    auto attr_value_it {values.begin()+2};
+
+    for (auto child : node_range(&doc))
+    {
+        CHECK(*elem_name_it++ == child->name_view());
+        CHECK(*elem_value_it++ == child->value_view());
+
+        for (auto attr : attribute_range(child))
+        {
+            CHECK(*attr_name_it++ == attr->name_view());
+            CHECK(*attr_value_it++ == attr->value_view());
+        }
+    }
+
+    return test::final_results();
+}
