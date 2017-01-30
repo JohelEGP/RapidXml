a48f50aad3d2ad4a17bfb192235e231ad6da5198
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2017-01-30 18:05:16 -0400

    [format] Apply Clang-Format

--- tests/test_ranges/main.cpp
+++ tests/test_ranges/main.cpp
@@ -1,17 +1,17 @@
+#include <vector>
 #include "../../rapidxml_iterators.hpp"
 #include "../test_utils.hpp"
-#include <vector>
 
 using namespace std;
 using namespace rapidxml;
 
 int main()
 {
-    vector<string_view> names {"elem1","elem2","attr1","attr2"};
-    vector<string_view> values {"elemval1","elemval2","attrval1","attrval2"};
+    vector<string_view> names{"elem1", "elem2", "attr1", "attr2"};
+    vector<string_view> values{"elemval1", "elemval2", "attrval1", "attrval2"};
 
-    auto name_it {names.begin()};
-    auto value_it {values.begin()};
+    auto name_it{names.begin()};
+    auto value_it{values.begin()};
 
     xml_document<char> doc;
     xml_node<char> *node1 = doc.allocate_node(node_element, *name_it++, *value_it++);
@@ -21,10 +21,10 @@ int main()
     doc.append_node(node1);
     doc.append_node(node2);
 
-    auto elem_name_it {names.begin()};
-    auto attr_name_it {names.begin()+2};
-    auto elem_value_it {values.begin()};
-    auto attr_value_it {values.begin()+2};
+    auto elem_name_it{names.begin()};
+    auto attr_name_it{names.begin() + 2};
+    auto elem_value_it{values.begin()};
+    auto attr_value_it{values.begin() + 2};
 
     for (auto child : node_range(&doc))
     {
